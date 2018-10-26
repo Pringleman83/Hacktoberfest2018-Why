5b580aa7cf52bf7c9dc32f053075f7da3e850e3c
Daniel <ecleticdaniel@gmail.com>
2018-10-26 10:52:34 +0100

    Added my reason ;)

---
+++ contributions/Daniel-Chibueze.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cctype>
+#include <cstring>
+
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	char why[200] = "I became a programmer because I simply want to become a software Engineer (*=x=*)";
+	cout << why;
+	return 0;
+}
